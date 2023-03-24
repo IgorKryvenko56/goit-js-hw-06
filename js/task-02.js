a9075f7b8a4352b1a9a163084224fbe1d52d5323
IgorKryvenko56 <ikryvenko@yahoo.co.uk>
2023-03-24 22:25:33 +0200

    Extra folder src was removed

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const itemEl = [];
+const listEl = document.querySelector("#ingredients");
+
+ingredients.map((ingredient) => {
+  const element = document.createElement("li");
+  element.textContent = ingredient;
+  element.classList.add("item");
+
+  itemEl.push(element);
+  listEl.append(element);
+});
